000551832987608aebfe58bd2bc13894e9e80af9
Daniel Moreh <daniel@woebot.io>
2020-04-23 12:43:21 -0700

    Bring window to front and activate this app

--- OpenQuickly/OpenQuicklyWindowController.swift
+++ OpenQuickly/OpenQuicklyWindowController.swift
@@ -37,12 +37,23 @@ open class OpenQuicklyWindowController: NSWindowController {
         }
     }
 
+    func show() {
+        guard let window = self.window else { return }
+
+        window.makeKeyAndOrderFront(self)
+        window.level = .floating
+        window.center()
+        window.orderFrontRegardless()
+        showWindow(self)
+
+        NSRunningApplication.current.activate(options: [.activateIgnoringOtherApps])
+    }
+
     public func toggle() {
         if self.windowIsVisible {
             self.close()
         } else {
-            window?.makeKeyAndOrderFront(self)
-            showWindow(self)
+            self.show()
         }
     }
 }
